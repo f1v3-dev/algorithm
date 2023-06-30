51a3a9dc23ec5b112ad141aabf4b5b93ffe44148
Seungjo, Jeong <f1v3@kakao.com>
2023-06-30 22:07:51 +0900

    [Silver III] Title: 후위 표기식2, Time: 136 ms, Memory: 14532 KB -BaekjoonHub

--- 백준/Silver/1935. 후위 표기식2/후위 표기식2.java
+++ 백준/Silver/1935. 후위 표기식2/후위 표기식2.java
@@ -6,47 +6,38 @@ import java.util.Stack;
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
         int num = Integer.parseInt(br.readLine());
-
         String input = br.readLine();
 
         Stack<Double> stack = new Stack<>();
-        Double [] arr = new Double[num];
+        Double[] arr = new Double[num];
         for (int i = 0; i < num; i++) {
             arr[i] = Double.parseDouble(br.readLine());
         }
 
-
-        for (int i = 0; i < input.length(); i++) {
-            char ch = input.charAt(i);
+        for (char ch : input.toCharArray()) {
             if (65 <= ch && ch <= 90) {
                 stack.push(arr[ch - 65]);
-            }
-
-            else if (ch == '+') {
+            } else {
                 double num2 = stack.pop();
                 double num1 = stack.pop();
-                stack.push(num1 + num2);
-
-            } else if (ch == '-') {
-                double num2 = stack.pop();
-                double num1 = stack.pop();
-                stack.push(num1 - num2);
-
-            } else if (ch == '*') {
-                double num2 = stack.pop();
-                double num1 = stack.pop();
-                stack.push(num1 * num2);
-
-            } else if (ch == '/') {
-                double num2 = stack.pop();
-                double num1 = stack.pop();
-                stack.push(num1 / num2);
+                switch (ch) {
+                    case '+':
+                        stack.push(num1 + num2);
+                        break;
+                    case '-':
+                        stack.push(num1 - num2);
+                        break;
+                    case '*':
+                        stack.push(num1 * num2);
+                        break;
+                    case '/':
+                        stack.push(num1 / num2);
+                        break;
+                }
             }
         }
 
         System.out.printf("%.2f", stack.pop());
-
     }
 }
