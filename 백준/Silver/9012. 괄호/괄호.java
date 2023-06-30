ebce392aff707272f14a0c28d95a1e7d6b2b5a01
Seungjo, Jeong <f1v3@kakao.com>
2023-06-30 16:34:56 +0900

    [Silver IV] Title: 괄호, Time: 128 ms, Memory: 14304 KB -BaekjoonHub

---
+++ 백준/Silver/9012. 괄호/괄호.java
@@ -0,0 +1,41 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Stack;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        int num = Integer.parseInt(br.readLine());
+
+        for (int i = 0; i < num; i++) {
+            String str = br.readLine();
+            if (check(str)) {
+                sb.append("YES").append("\n");
+            } else sb.append("NO").append("\n");
+        }
+
+        System.out.println(sb);
+    }
+
+    public static boolean check(String str) {
+
+        Stack<Character> stack = new Stack<>();
+
+        for (int i = 0; i < str.length(); i++) {
+            char ch = str.charAt(i);
+
+            if (ch == '(') {
+                stack.push(ch);
+            } else if (ch == ')') {
+                if (stack.isEmpty() || stack.peek() != '(') {
+                    return false;
+                }
+                stack.pop();
+            }
+        }
+        return stack.isEmpty();
+    }
+}
