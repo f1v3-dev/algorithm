5010ace33e114e7c1201ff39e73b3c077e6cee08
Seungjo, Jeong <f1v3@kakao.com>
2023-07-01 17:02:22 +0900

    [Silver II] Title: 스택 수열, Time: 368 ms, Memory: 27440 KB -BaekjoonHub

--- 백준/Silver/1874. 스택 수열/스택 수열.java
+++ 백준/Silver/1874. 스택 수열/스택 수열.java
@@ -1,35 +1,36 @@
-import java.util.*;
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Stack;
 
-public class Main{
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		StringBuilder sb = new StringBuilder();
-		
-		int N = sc.nextInt();
-		Stack<Integer> stack = new Stack<>();
-		int start = 0;
-		
-		while(N-- > 0) {
-			int num = sc.nextInt();
-			
-			if (num > start) {
-				for (int i = start + 1; i <= num; i++) {
-					stack.push(i);
-					sb.append("+").append("\n");
-				}
-				start = num;
-			}
-			else if (stack.peek() != num) {
-				sb.delete(0, sb.length());
-				sb.append("NO");
-				break;
-			}
-			
-			stack.pop();
-			sb.append("-").append("\n");
-		}
-		
-		System.out.println(sb);
-	}
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        int N = Integer.parseInt(br.readLine());
+        Stack<Integer> stack = new Stack<>();
+        int start = 0;
+        while (N-- > 0) {
+            int num = Integer.parseInt(br.readLine());
+
+            if (num > start) {
+                for (int i = start + 1; i <= num; i++){
+                    stack.push(i);
+                    sb.append("+").append("\n");
+                }
+                start = num;
+            }
+
+            if (stack.peek() != num) {
+                sb.delete(0, sb.length());
+                sb.append("NO");
+                break;
+            }
+
+            stack.pop();
+            sb.append("-").append("\n");
+        }
+        System.out.println(sb);
+    }
 }
