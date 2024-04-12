2bf54ce83a3d809b7690fe5b4c53f01113d1ce50
Seungjo, Jeong <f1v3@kakao.com>
2024-04-12 16:31:52 +0900

    [Silver IV] Title: 비밀번호 찾기, Time: 1456 ms, Memory: 67152 KB -BaekjoonHub

---
+++ 백준/Silver/17219. 비밀번호 찾기/비밀번호 찾기.java
@@ -0,0 +1,30 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.HashMap;
+import java.util.Map;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+
+        Map<String, String> map = new HashMap<>();
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String[] input = br.readLine().split(" ");
+
+        int N = Integer.parseInt(input[0]);
+        int M = Integer.parseInt(input[1]);
+
+        for (int i = 0; i < N; i++) {
+            input = br.readLine().split(" ");
+
+            map.put(input[0], input[1]);
+        }
+
+        for (int i = 0; i < M; i++) {
+            String site = br.readLine();
+            System.out.println(map.get(site));
+        }
+    }
+}
