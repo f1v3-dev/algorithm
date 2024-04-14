b24105e2ce73dac1c635f38fbc6178b17b74ac36
Seungjo, Jeong <f1v3@kakao.com>
2024-04-14 22:56:05 +0900

    [Gold V] Title: 별 찍기 - 10, Time: 396 ms, Memory: 48024 KB -BaekjoonHub

---
+++ 백준/Gold/2447. 별 찍기 － 10/별 찍기 － 10.java
@@ -0,0 +1,59 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    private static char[][] arr;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+
+        arr = new char[N][N];
+
+        star(0, 0, N, false);
+
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                sb.append(arr[i][j]);
+            }
+            sb.append('\n');
+        }
+        System.out.print(sb);
+    }
+
+    static void star(int x, int y, int N, boolean isBlank) {
+
+        if (isBlank) {
+            for (int i = x; i < x + N; i++) {
+                for (int j = y; j < y + N; j++) {
+                    arr[i][j] = ' ';
+                }
+            }
+            return;
+        }
+
+        // 더이상 나눠지지 않는 경우
+        if (N == 1) {
+            arr[x][y] = '*';
+            return;
+        }
+
+        // 3*3 블록으로 계속해서 나누기
+        int size = N / 3;
+        int count = 0;
+        
+        // count = 5 일 때, 가운데 블록은 공백처리
+        for (int i = x; i < x + N; i += size) {
+            for (int j = y; j < y + N; j += size) {
+                count++;
+                if (count == 5) { // 공백 칸일 경우
+                    star(i, j, size, true);
+                } else {
+                    star(i, j, size, false);
+                }
+            }
+        }
+    }
+}
