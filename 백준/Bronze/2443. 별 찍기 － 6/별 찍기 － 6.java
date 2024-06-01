37b1e6052c41413c17f85e7d511bdd4add99cd7b
Seungjo, Jeong <f1v3@kakao.com>
2024-06-01 15:25:26 +0900

    [Bronze III] Title: 별 찍기 - 6, Time: 192 ms, Memory: 17648 KB -BaekjoonHub

---
+++ 백준/Bronze/2443. 별 찍기 － 6/별 찍기 － 6.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int num = sc.nextInt();
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = num; i > 0; i--) {
+
+            // left
+            for (int j = 1; j <= num - i; j++) {
+                sb.append(" ");
+            }
+
+            // right
+            for (int j = 1; j <= 2 * i - 1; j++) {
+                sb.append("*");
+            }
+
+            sb.append("\n");
+        }
+
+        System.out.println(sb);
+        sc.close();
+    }
+}
