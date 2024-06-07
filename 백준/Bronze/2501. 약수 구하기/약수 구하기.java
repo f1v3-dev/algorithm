f6668adce48cd40934a29b112eb22accd0e06413
Seungjo, Jeong <f1v3@kakao.com>
2024-06-07 14:14:20 +0900

    [Bronze III] Title: 약수 구하기, Time: 124 ms, Memory: 14260 KB -BaekjoonHub

---
+++ 백준/Bronze/2501. 약수 구하기/약수 구하기.java
@@ -0,0 +1,30 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String[] input = br.readLine().split(" ");
+        int n = Integer.parseInt(input[0]);
+        int k = Integer.parseInt(input[1]);
+
+        List<Integer> list = new ArrayList<>();
+
+        for (int i = 1; i <= n; i++) {
+            if (n % i == 0) {
+                list.add(i);
+            }
+        }
+
+        int result = 0;
+        if (list.size() >= k) {
+            result = list.get(k - 1);
+        }
+
+        System.out.println(result);
+    }
+}
