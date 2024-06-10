495bbd20c18d90dac067cab63f36fdbb5b5e4c02
Seungjo, Jeong <f1v3@kakao.com>
2024-06-10 20:34:11 +0900

    [Gold II] Title: 2048 (Easy), Time: 220 ms, Memory: 19756 KB -BaekjoonHub

--- 백준/Gold/12100. 2048 （Easy）/2048 （Easy）.java
+++ 백준/Gold/12100. 2048 （Easy）/2048 （Easy）.java
@@ -6,15 +6,12 @@ public class Main {
 
     static int n;
     static int[][] arr;
-    static int[] dx = {0, 0, -1, 1};
-    static int[] dy = {1, -1, 0, 0};
     static int max = Integer.MIN_VALUE;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         n = Integer.parseInt(br.readLine());
-
         arr = new int[n][n];
 
         for (int i = 0; i < n; i++) {
@@ -29,221 +26,123 @@ public class Main {
     }
 
     private static void dfs(int count, int[][] copiedArray) {
-
         if (count == 5) {
-            for (int i = 0; i < n; i++) {
-                for (int j = 0; j < n; j++) {
-                    max = Math.max(max, copiedArray[i][j]);
-                }
-            }
-
+            updateMax(copiedArray);
             return;
         }
 
-
         for (int i = 0; i < 4; i++) {
             int[][] temp = new int[n][n];
-            for (int j = 0; j < n; j++) {
-                System.arraycopy(copiedArray[j], 0, temp[j], 0, n);
-            }
-
-            // 1. 상하좌우로 이동
-            // 2. 이동하는 방향으로 같은 숫자가 존재할 경우 합치기
-            // 3. 만약 변경사항이 없을 경우 -> 다음 방향으로 이동
-            // 4. 변경사항이 존재할 경우 -> 다음 dfs 호출
-            // 5. dfs 호출 후 원래 배열로 복귀
-
+            copyArray(copiedArray, temp);
             move(temp, i);
             dfs(count + 1, temp);
         }
     }
 
+    private static void updateMax(int[][] array) {
+        for (int[] row : array) {
+            for (int value : row) {
+                max = Math.max(max, value);
+            }
+        }
+    }
+
+    private static void copyArray(int[][] source, int[][] destination) {
+        for (int i = 0; i < n; i++) {
+            System.arraycopy(source[i], 0, destination[i], 0, n);
+        }
+    }
 
     private static void move(int[][] temp, int direction) {
+        boolean[][] merged = new boolean[n][n];
+        if (direction == 0) {
+            moveRight(temp, merged);
+        } else if (direction == 1) {
+            moveLeft(temp, merged);
+        } else if (direction == 2) {
+            moveUp(temp, merged);
+        } else if (direction == 3) {
+            moveDown(temp, merged);
+        }
+    }
 
-
-        // 1. 이동 방향에 0이 존재할 경우 해당 방향의 값들을 끝까지 이동시키기
-        // 2. 이동 방향에 같은 숫자가 존재할 경우 합치기
-        // 3. 합친 후 0이 존재할 경우 해당 방향의 값들을 끝까지 이동시키기
-
-        switch (direction) {
-            // right (y값 증가)
-            case 0:
-
-                // 2번
-                for (int i = 0; i < n; i++) {
-                    for (int j = n - 1; j >= 0; j--) {
-                        if (temp[i][j] != 0) {
-                            for (int k = j - 1; k >= 0; k--) {
-                                if (temp[i][k] == temp[i][j]) {
-                                    temp[i][j] *= 2;
-                                    temp[i][k] = 0;
-                                    break;
-                                } else if (temp[i][k] != 0) {
-                                    break;
-                                }
-                            }
-                        }
+    private static void moveRight(int[][] temp, boolean[][] merged) {
+        for (int i = 0; i < n; i++) {
+            for (int j = n - 1; j >= 0; j--) {
+                if (temp[i][j] != 0) {
+                    int k = j;
+                    while (k + 1 < n && temp[i][k + 1] == 0) {
+                        temp[i][k + 1] = temp[i][k];
+                        temp[i][k] = 0;
+                        k++;
+                    }
+                    if (k + 1 < n && temp[i][k + 1] == temp[i][k] && !merged[i][k + 1]) {
+                        temp[i][k + 1] *= 2;
+                        temp[i][k] = 0;
+                        merged[i][k + 1] = true;
                     }
                 }
+            }
+        }
+    }
 
-                // 3번
-                for (int i = 0; i < n; i++) {
-                    for (int j = n - 1; j >= 0; j--) {
-                        if (temp[i][j] == 0) {
-                            for (int k = j - 1; k >= 0; k--) {
-                                if (temp[i][k] != 0) {
-                                    temp[i][j] = temp[i][k];
-                                    temp[i][k] = 0;
-                                    break;
-                                }
-                            }
-                        }
+    private static void moveLeft(int[][] temp, boolean[][] merged) {
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (temp[i][j] != 0) {
+                    int k = j;
+                    while (k - 1 >= 0 && temp[i][k - 1] == 0) {
+                        temp[i][k - 1] = temp[i][k];
+                        temp[i][k] = 0;
+                        k--;
+                    }
+                    if (k - 1 >= 0 && temp[i][k - 1] == temp[i][k] && !merged[i][k - 1]) {
+                        temp[i][k - 1] *= 2;
+                        temp[i][k] = 0;
+                        merged[i][k - 1] = true;
                     }
                 }
-                break;
+            }
+        }
+    }
 
-            // left (y값 감소)
-            case 1:
-                for (int i = 0; i < n; i++) {
-                    for (int j = 0; j < n; j++) {
-                        if (temp[i][j] == 0) {
-                            for (int k = j + 1; k < n; k++) {
-                                if (temp[i][k] != 0) {
-                                    temp[i][j] = temp[i][k];
-                                    temp[i][k] = 0;
-                                    break;
-                                }
-
-                            }
-                        }
+    private static void moveUp(int[][] temp, boolean[][] merged) {
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (temp[j][i] != 0) {
+                    int k = j;
+                    while (k - 1 >= 0 && temp[k - 1][i] == 0) {
+                        temp[k - 1][i] = temp[k][i];
+                        temp[k][i] = 0;
+                        k--;
+                    }
+                    if (k - 1 >= 0 && temp[k - 1][i] == temp[k][i] && !merged[k - 1][i]) {
+                        temp[k - 1][i] *= 2;
+                        temp[k][i] = 0;
+                        merged[k - 1][i] = true;
                     }
                 }
+            }
+        }
+    }
 
-                for (int i = 0; i < n; i++) {
-                    for (int j = 0; j < n; j++) {
-                        if (temp[i][j] != 0) {
-                            for (int k = j + 1; k < n; k++) {
-                                if (temp[i][k] == temp[i][j]) {
-                                    temp[i][j] *= 2;
-                                    temp[i][k] = 0;
-                                    break;
-                                } else if (temp[i][k] != 0) {
-                                    break;
-                                }
-                            }
-                        }
+    private static void moveDown(int[][] temp, boolean[][] merged) {
+        for (int i = 0; i < n; i++) {
+            for (int j = n - 1; j >= 0; j--) {
+                if (temp[j][i] != 0) {
+                    int k = j;
+                    while (k + 1 < n && temp[k + 1][i] == 0) {
+                        temp[k + 1][i] = temp[k][i];
+                        temp[k][i] = 0;
+                        k++;
+                    }
+                    if (k + 1 < n && temp[k + 1][i] == temp[k][i] && !merged[k + 1][i]) {
+                        temp[k + 1][i] *= 2;
+                        temp[k][i] = 0;
+                        merged[k + 1][i] = true;
                     }
                 }
-
-                for (int i = 0; i < n; i++) {
-                    for (int j = 0; j < n; j++) {
-                        if (temp[i][j] == 0) {
-                            for (int k = j + 1; k < n; k++) {
-                                if (temp[i][k] != 0) {
-                                    temp[i][j] = temp[i][k];
-                                    temp[i][k] = 0;
-                                    break;
-                                }
-                            }
-                        }
-                    }
-                }
-                break;
-
-            // up (x값 감소)
-            case 2:
-                for (int i = 0; i < n; i++) {
-                    for (int j = 0; j < n; j++) {
-                        if (temp[j][i] == 0) {
-                            for (int k = j + 1; k < n; k++) {
-                                if (temp[k][i] != 0) {
-                                    temp[j][i] = temp[k][i];
-                                    temp[k][i] = 0;
-                                    break;
-                                }
-                            }
-                        }
-                    }
-                }
-
-                for (int i = 0; i < n; i++) {
-                    for (int j = 0; j < n; j++) {
-                        if (temp[j][i] != 0) {
-                            for (int k = j + 1; k < n; k++) {
-                                if (temp[k][i] == temp[j][i]) {
-                                    temp[j][i] *= 2;
-                                    temp[k][i] = 0;
-                                    break;
-                                } else if (temp[k][i] != 0) {
-                                    break;
-                                }
-                            }
-                        }
-                    }
-                }
-
-                for (int i = 0; i < n; i++) {
-                    for (int j = 0; j < n; j++) {
-                        if (temp[j][i] == 0) {
-                            for (int k = j + 1; k < n; k++) {
-                                if (temp[k][i] != 0) {
-                                    temp[j][i] = temp[k][i];
-                                    temp[k][i] = 0;
-                                    break;
-                                }
-                            }
-                        }
-                    }
-                }
-                break;
-
-            // down (x값 증가)
-            case 3:
-                for (int i = 0; i < n; i++) {
-                    for (int j = n - 1; j >= 0; j--) {
-                        if (temp[j][i] == 0) {
-                            for (int k = j - 1; k >= 0; k--) {
-                                if (temp[k][i] != 0) {
-                                    temp[j][i] = temp[k][i];
-                                    temp[k][i] = 0;
-                                    break;
-                                }
-                            }
-                        }
-                    }
-                }
-
-                for (int i = 0; i < n; i++) {
-                    for (int j = n - 1; j >= 0; j--) {
-                        if (temp[j][i] != 0) {
-                            for (int k = j - 1; k >= 0; k--) {
-                                if (temp[k][i] == temp[j][i]) {
-                                    temp[j][i] *= 2;
-                                    temp[k][i] = 0;
-                                    break;
-                                } else if (temp[k][i] != 0) {
-                                    break;
-                                }
-                            }
-                        }
-                    }
-                }
-
-                for (int i = 0; i < n; i++) {
-                    for (int j = n - 1; j >= 0; j--) {
-                        if (temp[j][i] == 0) {
-                            for (int k = j - 1; k >= 0; k--) {
-                                if (temp[k][i] != 0) {
-                                    temp[j][i] = temp[k][i];
-                                    temp[k][i] = 0;
-                                    break;
-                                }
-                            }
-                        }
-                    }
-                }
-                break;
+            }
         }
     }
 }
