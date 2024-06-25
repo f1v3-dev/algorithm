762916a4b6ce6247f0c7f04f7d54957d03c31096
Seungjo, Jeong <f1v3@kakao.com>
2024-06-25 13:44:20 +0900

    [Silver II] Title: 가장 긴 증가하는 부분 수열, Time: 136 ms, Memory: 14356 KB -BaekjoonHub

--- 백준/Silver/11053. 가장 긴 증가하는 부분 수열/가장 긴 증가하는 부분 수열.java
+++ 백준/Silver/11053. 가장 긴 증가하는 부분 수열/가장 긴 증가하는 부분 수열.java
@@ -5,15 +5,16 @@ import java.util.StringTokenizer;
 
 public class Main {
 
+    static int n;
     static int[] arr;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        int n = Integer.parseInt(br.readLine());
+        n = Integer.parseInt(br.readLine());
         arr = new int[n];
 
-        StringTokenizer st = new StringTokenizer(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
         for (int i = 0; i < n; i++) {
             arr[i] = Integer.parseInt(st.nextToken());
         }
@@ -23,26 +24,51 @@ public class Main {
 
     private static int solve() {
 
-        // 가장 긴 증가하는 부분 수열
-        int[] dp = new int[arr.length];
-        dp[0] = 1;
+        // LIS 방식으로 풀어보자
+        // 가장 긴 증가하는 부분 수열을 만들어가는 과정
 
-        int maxSequence = dp[0];
-        for (int i = 1; i < arr.length; i++) {
-            dp[i] = 1;
+        int[] LIS = new int[n];
+        LIS[0] = arr[0];
 
-            // i 이전의 값들을 하나씩 꺼내서 비교
-            for (int j = 0; j < i; j++) {
+        int index = 1;
+        for (int i = 1; i < n; i++) {
 
-                // 1. 수열이 증가하는가?
-                if (arr[i] > arr[j] && dp[j] + 1 > dp[i]) {
-                    dp[i] = dp[j] + 1;
-                }
+            // 1. 현재 숫자가 LIS의 마지막 숫자보다 크다면
+            // LIS 배열에 추가
+            if (arr[i] > LIS[index - 1]) {
+                LIS[index++] = arr[i];
             }
 
-            maxSequence = Math.max(maxSequence, dp[i]);
+            // 2. 현재 숫자가 LIS의 첫 번째 숫자보다 작다면
+            // LIS 배열의 첫 번째 수를 arr[i]로 변경한다.
+            else if (LIS[0] > arr[i]) {
+                LIS[0] = arr[i];
+            }
+
+            // 3. binary search를 사용하여 LIS 배열에 들어갈 위치를 찾는다.
+            else {
+                int pivot = binarySearch(LIS, arr[i], 0, index);
+                LIS[pivot] = arr[i];
+            }
         }
 
-        return maxSequence;
+        return index;
+    }
+
+    private static int binarySearch(int[] LIS, int target, int start, int end) {
+
+        // binary search 시작
+        while (start < end) {
+
+            int mid = (start + end) / 2;
+
+            if (LIS[mid] < target) {
+                start = mid + 1;
+            } else {
+                end = mid;
+            }
+        }
+
+        return end;
     }
 }
