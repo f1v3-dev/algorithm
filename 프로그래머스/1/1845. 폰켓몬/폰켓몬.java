17130a6fc0ad12e4d60ced3798dfa2cf81703141
Seungjo <f1v3@kakao.com>
2024-09-24 23:07:58 +0900

    [level 1] Title: 폰켓몬, Time: 4.52 ms, Memory: 86.3 MB -BaekjoonHub

--- 프로그래머스/1/1845. 폰켓몬/폰켓몬.java
+++ 프로그래머스/1/1845. 폰켓몬/폰켓몬.java
@@ -1,14 +1,16 @@
+import java.util.Set;
 import java.util.HashSet;
 
 class Solution {
     public int solution(int[] nums) {
-        HashSet<Integer> set = new HashSet<>();
+
+        Set<Integer> set = new HashSet<>();
+        
         for (int num : nums) {
             set.add(num);
         }
         
         int size = nums.length / 2;
-        
         return Math.min(set.size(), size);
     }
 }
