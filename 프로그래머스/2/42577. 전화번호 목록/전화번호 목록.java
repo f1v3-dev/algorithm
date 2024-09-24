a3b7f7a9ac66ea32fe53f9f7436718ec606c7de8
Seungjo <f1v3@kakao.com>
2024-09-24 23:17:26 +0900

    [level 2] Title: 전화번호 목록, Time: 368.99 ms, Memory: 99.4 MB -BaekjoonHub

--- 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.java
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.java
@@ -1,14 +1,20 @@
-import java.util.Arrays;
+import java.util.*;
 
 class Solution {
     public boolean solution(String[] phone_book) {
+        // 접두어 -> sort
         
         Arrays.sort(phone_book);
-        for (int i = 0; i < phone_book.length - 1; i++) {
-            if (phone_book[i + 1].startsWith(phone_book[i])) {
+        
+        for (int i = 1; i < phone_book.length; i++) {
+            String prev = phone_book[i - 1];
+            String current = phone_book[i];
+            
+            if (current.startsWith(prev)) {
                 return false;
             }
         }
+        
         return true;
     }
 }
