c695936b1cd8ee378f0f779583a33b88d78d223c
Seungjo <f1v3@kakao.com>
2024-09-25 13:15:00 +0900

    [level 1] Title: K번째수, Time: 0.50 ms, Memory: 76 MB -BaekjoonHub

--- 프로그래머스/1/42748. K번째수/K번째수.java
+++ 프로그래머스/1/42748. K번째수/K번째수.java
@@ -4,14 +4,22 @@ class Solution {
     public int[] solution(int[] array, int[][] commands) {
         int[] answer = new int[commands.length];
 
-        int count = 0;
-        for (int[] command : commands) {
-            int[] temp = Arrays.copyOfRange(array, command[0] - 1, command[1]);
-            
-            Arrays.sort(temp);
-            answer[count++] = temp[command[2] - 1];
+        for (int i = 0; i < answer.length; i++) {
+            int[] command = commands[i];
+            answer[i] = findNumber(array, command);
         }
-        
+
         return answer;
+    }
+
+    private int findNumber(int[] array, int[] command) {
+        int start = command[0] - 1;
+        int end = command[1];
+        int pivot = command[2] - 1;
+
+        int[] temp = Arrays.copyOfRange(array, start, end);
+        Arrays.sort(temp);
+
+        return temp[pivot];
     }
 }
