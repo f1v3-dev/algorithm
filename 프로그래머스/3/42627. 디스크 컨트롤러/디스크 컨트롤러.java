4f5066e1073423bb537a8b30ea82d5611d4de63f
Seungjo <f1v3@kakao.com>
2024-09-25 12:41:12 +0900

    [level 3] Title: 디스크 컨트롤러, Time: 8.20 ms, Memory: 69.8 MB -BaekjoonHub

--- 프로그래머스/3/42627. 디스크 컨트롤러/디스크 컨트롤러.java
+++ 프로그래머스/3/42627. 디스크 컨트롤러/디스크 컨트롤러.java
@@ -1,54 +1,49 @@
+import java.util.Comparator;
 import java.util.PriorityQueue;
-
-class Process {
-    private int requestTime;
-    private int proceedTime;
-
-    public Process(int requestTime, int proceedTime) {
-        this.requestTime = requestTime;
-        this.proceedTime = proceedTime;
-    }
-
-    public int getRequestTime() {
-        return requestTime;
-    }
-
-    public int getProceedTime() {
-        return proceedTime;
-    }
-}
+import java.util.Queue;
 
 class Solution {
+
+    private static class Job {
+        int requestTime;
+        int processTime;
+
+        public Job(int requestTime, int processTime) {
+            this.requestTime = requestTime;
+            this.processTime = processTime;
+        }
+
+    }
+
     public int solution(int[][] jobs) {
 
 
-        PriorityQueue<Process> requestQueue = new PriorityQueue<>((p1, p2) -> p1.getRequestTime() - p2.getRequestTime());
-        PriorityQueue<Process> proceedQueue = new PriorityQueue<>((p1, p2) -> p1.getProceedTime() - p2.getProceedTime());
-
+        Queue<Job> waitingQueue = new PriorityQueue<>(Comparator.comparingInt(j -> j.requestTime));
+        Queue<Job> readyQueue = new PriorityQueue<>(Comparator.comparingInt(j -> j.processTime));
 
         for (int[] job : jobs) {
-            Process process = new Process(job[0], job[1]);
-            requestQueue.offer(process);
+            waitingQueue.offer(new Job(job[0], job[1]));
         }
 
-        int time = 0;
-        int answer = 0;
+        int currentTime = 0;
+        int totalTime = 0;
 
-        while (!requestQueue.isEmpty() || !proceedQueue.isEmpty()) {
+        while (!waitingQueue.isEmpty() || !readyQueue.isEmpty()) {
 
-            while (!requestQueue.isEmpty() && requestQueue.peek().getRequestTime() <= time) {
-                proceedQueue.offer(requestQueue.poll());
+            while (!waitingQueue.isEmpty() && waitingQueue.peek().requestTime <= currentTime) {
+                readyQueue.offer(waitingQueue.poll());
             }
 
-            if (proceedQueue.isEmpty()) {
-                time = requestQueue.peek().getRequestTime();
+            if (readyQueue.isEmpty()) {
+                currentTime = waitingQueue.peek().requestTime;
             } else {
-                Process process = proceedQueue.poll();
-                time += process.getProceedTime();
-                answer = answer + time - process.getRequestTime();
+                Job job = readyQueue.poll();
+                currentTime += job.processTime;
+                totalTime += currentTime - job.requestTime;
             }
         }
 
-        return answer /= jobs.length;
+
+        return totalTime / jobs.length;
     }
 }
