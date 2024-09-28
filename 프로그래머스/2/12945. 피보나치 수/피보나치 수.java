295daaf17f389da7b2c05a9f12bd652f3bb990cc
Seungjo <f1v3@kakao.com>
2024-09-28 10:12:35 +0900

    [level 2] Title: 피보나치 수, Time: 2.52 ms, Memory: 75.9 MB -BaekjoonHub

---
+++ 프로그래머스/2/12945. 피보나치 수/피보나치 수.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int solution(int n) {
+        return fibo(n);
+    }
+
+    private int fibo(int n) {
+        int[] fibo = new int[n + 1];
+
+        fibo[0] = 0;
+        fibo[1] = 1;
+
+        for (int i = 2; i <= n; i++) {
+            fibo[i] = (fibo[i - 1] + fibo[i - 2]) % 1234567;
+        }
+
+        return fibo[n];
+    }
+}
