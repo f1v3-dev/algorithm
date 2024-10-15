56d9ca15c3e902be0ceebe0696b8f7bf28edb5e9
Seungjo <f1v3@kakao.com>
2024-10-15 13:37:02 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 82.57 ms, Memory: 95.5 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
@@ -2,22 +2,27 @@ import java.util.*;
 
 class Solution {
     public String solution(String[] participant, String[] completion) {
+        String answer = "";
+        
         Map<String, Integer> map = new HashMap<>();
         
-        for (String name : participant) {
-            map.put(name, map.getOrDefault(name, 0) + 1);
+        for (String part : participant) {
+            map.put(part, map.getOrDefault(part, 0) + 1);
         }
         
-        for (String name : completion) {
-            map.put(name, map.get(name) - 1);
-        }
-        
-        return map.entrySet()
-            .stream()
-            .filter(e -> e.getValue() > 0)
-            .findFirst()
-            .map(e -> e.getKey())
-            .orElse(null);
+        for (String comp : completion) {
+            Integer count = map.get(comp);
             
+            if (count == 1) {
+                map.remove(comp);
+            } else {
+                map.put(comp, map.get(comp) - 1);
+            }
+        }
+        
+        return map.keySet()
+            .stream()
+            .findFirst()
+            .orElse("");
     }
 }
