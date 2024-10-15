5bcc9ba24834f8e5e360f1f8f1437fba1a1809c3
Seungjo <f1v3@kakao.com>
2024-10-15 13:29:32 +0900

    [level 1] Title: 폰켓몬, Time: 4.91 ms, Memory: 78.3 MB -BaekjoonHub

--- 프로그래머스/1/1845. 폰켓몬/README.md
+++ 프로그래머스/1/1845. 폰켓몬/README.md
@@ -1,10 +1,10 @@
 # [level 1] 폰켓몬 - 1845 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/1845) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/1845?language=java) 
 
 ### 성능 요약
 
-메모리: 86.3 MB, 시간: 4.52 ms
+메모리: 78.3 MB, 시간: 4.91 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 09월 24일 23:07:57
+2024년 10월 15일 13:29:29
 
 ### 문제 설명
 
--- 프로그래머스/1/1845. 폰켓몬/폰켓몬.java
+++ 프로그래머스/1/1845. 폰켓몬/폰켓몬.java
@@ -1,16 +1,15 @@
-import java.util.Set;
-import java.util.HashSet;
+import java.util.*;
 
 class Solution {
     public int solution(int[] nums) {
-
+        
         Set<Integer> set = new HashSet<>();
+        int len = nums.length / 2;
         
         for (int num : nums) {
             set.add(num);
         }
         
-        int size = nums.length / 2;
-        return Math.min(set.size(), size);
+        return set.size() > len ? len : set.size();
     }
 }
