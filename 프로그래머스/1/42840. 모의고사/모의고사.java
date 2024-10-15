30c2281c251d5d4a24184189c1e5659d22cebc3a
Seungjo <f1v3@kakao.com>
2024-10-15 16:26:15 +0900

    [level 1] Title: 모의고사, Time: 3.63 ms, Memory: 82.1 MB -BaekjoonHub

--- 프로그래머스/1/42840. 모의고사/모의고사.java
+++ 프로그래머스/1/42840. 모의고사/모의고사.java
@@ -1,40 +1,41 @@
 import java.util.*;
 
 class Solution {
+    
+    static int[] a = {1, 2, 3, 4, 5};
+    static int[] b = {2, 1, 2, 3, 2, 4, 2, 5};
+    static int[] c = {3, 3, 1, 1, 2, 2, 4, 4, 5, 5};
+    
     public int[] solution(int[] answers) {
-        // 1번 수포자 : 1, 2, 3, 4, 5 반복
-        // 2번 수포자 : 2, 1, 2, 3, 2, 4, 2, 5 반복
-        // 3번 수포자 : 3, 3, 1, 1, 2, 2, 4, 4, 5, 5 반복
-
-        int[] first = {1, 2, 3, 4, 5};
-        int[] second = {2, 1, 2, 3, 2, 4, 2, 5};
-        int[] third = {3, 3, 1, 1, 2, 2, 4, 4, 5, 5};
-
-        int[] score = new int[3]; // 각 수포자가 맞힌 문제 수를 저장할 배열
-
+        int[] count = new int[3];
+        
         for (int i = 0; i < answers.length; i++) {
-            if (answers[i] == first[i % first.length]) {
-                score[0]++;
+            int answer = answers[i];
+            
+            if (a[i % a.length] == answer) {
+                count[0]++;
             }
-            if (answers[i] == second[i % second.length]) {
-                score[1]++;
+            
+            if (b[i % b.length] == answer) {
+                count[1]++;
             }
-            if (answers[i] == third[i % third.length]) {
-                score[2]++;
-            }
-        }
-
-        int maxScore = Math.max(score[0], Math.max(score[1], score[2]));
-
-        List<Integer> list = new ArrayList<>();
-        for (int i = 0; i < score.length; i++) {
-            if (score[i] == maxScore) {
-                list.add(i + 1);
+            
+            if (c[i % c.length] == answer) {
+                count[2]++;
             }
         }
-
-        return list.stream()
-                .mapToInt(i -> i)
-                .toArray();
+        
+        int max = Math.max(count[0], Math.max(count[1], count[2]));
+            
+        List<Integer> result = new ArrayList<>();
+        for (int i = 0; i < count.length; i++) {
+            if (count[i] == max) {
+                result.add(i + 1);
+            }
+        }
+        
+        return result.stream()
+            .mapToInt(i -> i)
+            .toArray();
     }
 }
