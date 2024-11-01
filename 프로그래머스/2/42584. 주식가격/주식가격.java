9820ae78dd463017d3b2c1f1f58ef51d893e7915
Seungjo <f1v3@kakao.com>
2024-11-01 14:39:19 +0900

    [level 2] Title: 주식가격, Time: 19.19 ms, Memory: 69.2 MB -BaekjoonHub

--- 프로그래머스/2/42584. 주식가격/주식가격.java
+++ 프로그래머스/2/42584. 주식가격/주식가격.java
@@ -1,17 +1,11 @@
-import java.util.*;
-
 class Solution {
     public int[] solution(int[] prices) {
 
-        Queue<Integer> queue = new LinkedList<>();
-        for (int price : prices) {
-            queue.offer(price);
-        }
 
         int[] answer = new int[prices.length];
 
         for (int i = 0; i < prices.length; i++) {
-            int price = queue.poll();
+            int price = prices[i];
             int count = 0;
             for (int j = i + 1; j < prices.length; j++) {
                 count++;
