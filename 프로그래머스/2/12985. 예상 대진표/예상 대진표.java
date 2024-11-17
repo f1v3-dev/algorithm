84f52db8f6a72e710e7829c5d9a2ef8e5b93e2ca
Seungjo <f1v3@kakao.com>
2024-11-17 18:53:23 +0900

    [level 2] Title: 예상 대진표, Time: 0.03 ms, Memory: 75.9 MB -BaekjoonHub

---
+++ 프로그래머스/2/12985. 예상 대진표/예상 대진표.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int solution(int n, int a, int b) {
+
+        int answer = 0;
+
+        // a == b 인 경우 만난다.
+        while (a != b) {
+
+            a = (a + 1) / 2;
+            b = (b + 1) / 2;
+
+            answer++;
+        }
+
+        return answer;
+    }
+}
