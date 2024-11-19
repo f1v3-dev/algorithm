d4796fb79853e3c022b9a138efd70c047b5396f9
Seungjo <f1v3@kakao.com>
2024-11-19 14:14:17 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 11.29 ms, Memory: 53.2 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
@@ -1,43 +1,53 @@
-import java.util.*;
+import java.util.LinkedList;
+import java.util.Queue;
+
+class Position {
+    int x;
+    int y;
+    int count;
+
+    public Position(int x, int y, int count) {
+        this.x = x;
+        this.y = y;
+        this.count = count;
+    }
+}
 
 class Solution {
-
     static int[] dx = {0, 0, -1, 1};
     static int[] dy = {-1, 1, 0, 0};
 
     public int solution(int[][] maps) {
-        return bfs(maps, 0, 0);
+        return bfs(maps);
     }
 
-    private int bfs(int[][] maps, int x, int y) {
-        int n = maps.length;
-        int m = maps[0].length;
+    private int bfs(int[][] maps) {
+        Queue<Position> queue = new LinkedList<>();
+        boolean[][] visited = new boolean[maps.length][maps[0].length];
+        visited[0][0] = true;
 
-        Queue<int[]> queue = new LinkedList<>();
-        queue.offer(new int[]{x, y, 0});
-
-        boolean[][] visited = new boolean[n][m];
-        visited[x][y] = true;
+        queue.offer(new Position(0, 0, 1));
 
         while (!queue.isEmpty()) {
-            int[] cur = queue.poll();
-            int cx = cur[0];
-            int cy = cur[1];
-            int count = cur[2] + 1;
+            Position cur = queue.poll();
+            int cx = cur.x;
+            int cy = cur.y;
+            int count = cur.count;
 
             for (int i = 0; i < 4; i++) {
                 int nx = cx + dx[i];
                 int ny = cy + dy[i];
 
-                if (nx >= 0 && nx < n && ny >= 0 && ny < m) {
-                    if (maps[nx][ny] == 1 && !visited[nx][ny]) {
+                if (nx >= 0 && nx < maps.length && ny >= 0 && ny < maps[0].length
+                        && !visited[nx][ny] && maps[nx][ny] == 1) {
 
-                        if (nx == n - 1 && ny == m - 1) {
-                            return count + 1;
-                        }
-                        visited[nx][ny] = true;
-                        queue.offer(new int[]{nx, ny, count});
+                    // 도착지에 도달했을 경우 return
+                    if (nx == maps.length - 1 && ny == maps[0].length - 1) {
+                        return count + 1;
                     }
+
+                    visited[nx][ny] = true;
+                    queue.offer(new Position(nx, ny, count + 1));
                 }
             }
         }
